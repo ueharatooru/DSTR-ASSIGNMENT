7a43deb24fd6c5a24bb43515629b4736de5df9bb
ueharatooru <kamikaze1098@yahoo.com>
2022-05-29 18:27:00 +0900

    Commit

--- New/Test 2/Test 2/addtutor.cpp
+++ New/Test 2/Test 2/addtutor.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 int T = 0;
 string Tutor[100][11];
-<<<<<<< HEAD
 
 void display_tutor() {
     for (int i = 0; i < T; i++) {
         for (int j = 0; j < 11; j++) {
-=======
+            if (j == 10) {
+                cout << Tutor[i][j] << endl;
+            }
+            else { cout << Tutor[i][j] << ", "; }
+        }
+    }
 
+    action_page();
+}
 void add_tutor() {
-        
+
+    
         cout << "Enter your ID: ";
         cin >> Tutor[T][0];
         cout << "Enter your Name: ";
@@ -36,7 +43,7 @@ void add_tutor() {
         cout << "Enter your subject name: ";
         cin >> Tutor[T][10];
 
-        cout << Tutor[T];
+        
         T++;
 
         while (true) {
@@ -53,71 +60,22 @@ void add_tutor() {
 
 void display_tutor() {
     for (int i = 0; i < T; i++) {
-        for (int j = 0; j < 10; j++) {
->>>>>>> 1c6c8db036d68b9b3644812da401b50f1971c430
+        for (int j = 0; j < 10; i++) {
             if (j == 10) {
                 cout << Tutor[i][j] << endl;
             }
-            else { cout << Tutor[i][j] << ", "; }
+            cout << Tutor[i][j];
         }
     }
-<<<<<<< HEAD
 
-    action_page();
-}
-void add_tutor() {
+    int action;
+    cout << "Choose your action: 1) add tutor, 2) Display all tutors: ";
+    cin >> action;
+    if (action == 1) {
 
-    cout << "Enter your ID: ";
-    cin >> Tutor[T][0];
-    cout << "Enter your Name: ";
-    cin >> Tutor[T][1];
-    cout << "Enter your date join(yyyy/mm/dd): ";
-    cin >> Tutor[T][2];
-    cout << "Enter your date terminal(yyyy/mm/dd): ";
-    cin >> Tutor[T][3];
-    cout << "Enter your hourly paid: ";
-    cin >> Tutor[T][4];
-    cout << "Enter your phone: ";
-    cin >> Tutor[T][5];
-    cout << "Enter your address: ";
-    cin >> Tutor[T][6];
-    cout << "Enter your tuition code: ";
-    cin >> Tutor[T][7];
-    cout << "Enter your tuition center name: ";
-    cin >> Tutor[T][8];
-    cout << "Enter your subject code: ";
-    cin >> Tutor[T][9];
-    cout << "Enter your subject name: ";
-    cin >> Tutor[T][10];
-
-
-    T++;
-    
-    while (true) {
-        string choose;
-        cout << "=========================================================================" << endl;
-        cout << "Continue add?(y/n): ";
-        cin >> choose;
-        if (choose == "y" || choose == "Y") {
-            
-            add_tutor();
-
-        }
-        else if (choose == "n" || choose == "N") {
-            
-            action_page();
-        }
-        else {
-            cout << "only y/n" << endl;
-        }
+        add_tutor();
     }
-    
-   
-    
-
-
+    if (action == 2) {
+        display_tutor();
+    }
 }
-
-=======
-}
->>>>>>> 1c6c8db036d68b9b3644812da401b50f1971c430
